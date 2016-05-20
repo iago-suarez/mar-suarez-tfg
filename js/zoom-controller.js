10b8c25ff288337b65a3e7ef974474f1c07189fb
Xoel <Xoel@Xoel-PC>
2016-05-20 16:58:20 +0200

    Añadido tanto el pantalla completa, como el radiobutton para seleccionar que video vemos.

--- js/zoom-controller.js
+++ js/zoom-controller.js
@@ -3,7 +3,9 @@
  */
 
 //This is the size of the first window, this was set manualliy
-var TOP_LEFT_POINT = {x: 663, y: 18}; //Pixels
+var TOP_LEFT_POINT = {x: 663, y: 18}; //Pixels video 1
+//var TOP_LEFT_POINT = {x: 663, y: 370}; //Pixels video 2
+//var TOP_LEFT_POINT = {x: 663, y: 718}; //Pixels video 3
 var VIDEO_WINDOW_SIZE = {w: 595, h: 335}; //Pixels (Note: You have to respect the aspect ratio)
 
 /********************************************************************************/
@@ -11,6 +13,9 @@ var video;
 var loop_running = false;
 var systemStarted = false;
 var my_manager;
+//That variable select which video is focus on
+var videoNumber = 1;
+var fullScreenOn;
 
 var updateIndex = 0;
 var lastUpdateTimes = Array.apply(null, new Array(10)).map(Number.prototype.valueOf, 0);
@@ -27,14 +32,17 @@ function arrayMean(arr) {
 
 /**
  * If the video is runnig, call to the my_manager.updateState() for each frame.
+ * Here we should control if fullsize or not
  */
 function updateStatus() {
+    //TODO fix fullsize
     if (!video.paused) {
         loop_running = true;
-
         var time = Date.now();
         my_manager.updateState();
-
+        //NO deberiamos ajustar aqui el canvas por que lo hace
+        //ochenta veces por segundo, arreglarlo en resize
+        adaptCanvas();
         updateIndex = (updateIndex + 1) % lastUpdateTimes.length;
         lastUpdateTimes[updateIndex] = Date.now() - time;
         //Waits the fps time least data processing time
@@ -47,22 +55,26 @@ function updateStatus() {
     }
 }
 
+
+
 /**
  * Set the canvas element size to be the video size
+ * @param {type} video
+ * @returns {undefined}
  */
 function adjustCanvas(video) {
     // This container define the real size of the canvas element, because
     // it has the css widt and height set to 100%
     $('#canvas-container')
-        .height($(video).height())
-        .width($(video).width())
-        .offset({top: $(video).offset().top, left: $(video).offset().left});
+            .height($(video).height())
+            .width($(video).width())
+            .offset({top: $(video).offset().top, left: $(video).offset().left});
 
     // This attribute se the size of the canvas content
     // NOTE: The aspect ratio must respect the video aspect ratio
     $('.drawing-layer')
-        .attr('height', VIDEO_WINDOW_SIZE.h)
-        .attr('width', VIDEO_WINDOW_SIZE.w);
+            .attr('height', VIDEO_WINDOW_SIZE.h)
+            .attr('width', VIDEO_WINDOW_SIZE.w);
 
     my_manager.updateState();
 }
@@ -89,12 +101,14 @@ function bindVideoEvents(video) {
 
 function ZoomManager(videoElement, canvasElement) {
     this.videoElement = videoElement;
-    this.canvasElement = canvasElement
+    this.canvasElement = canvasElement;
 
     /**
      * Updates the zoom state in function of the time and the detection position
      */
     this.updateState = function () {
+        //seleccionamos el video que toca ver segun el radiobutton
+        selectVideo();
         var context = this.canvasElement.getContext('2d');
         context.fillStyle = 'black';
         //The image to draw will fill the full canvas size
@@ -103,8 +117,53 @@ function ZoomManager(videoElement, canvasElement) {
     };
 }
 
-$(document).ready(function () {
+/**
+ * adapt the cambas to fullscreem or normal screem
+ * 
+ * @returns {undefined}
+ */
+function adaptCanvas() {
+    fullScreenOn = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+    if (fullScreenOn) {
+        // browser is fullscreen
+        //adapta el cambas al tamaño, dandole la profuncidad neesaria en el CSS
+        $('.drawing-layer').css('z-index', 2147483647);
+    } else {
+        $('.drawing-layer').css('z-index', "");
+    }
+}
+/**
+ * choose the value of radiobutton
+ * @param {type} ctrl
+ * @returns {unresolved}
+ */
+function getRadioButtonSelectedValue(ctrl) {
+    for (i = 0; i < ctrl.length; i++)
+        if (ctrl[i].checked)
+            return ctrl[i].value;
+}
+/**
+ * Select the video to see with the value of radiobutton.
+ * @returns {undefined}
+ */
+function selectVideo() {
+    videoNumber = getRadioButtonSelectedValue(document.fVideos.video);
+    //console.log(videoNumber);
+    if (parseInt(videoNumber) === 1) {
+        //console.log("Entramos en 1");
+        TOP_LEFT_POINT = {x: 663, y: 18}; //Pixels video 1
+    } else if (parseInt(videoNumber) === 2) {
+        //console.log("Entramos en ");
+        TOP_LEFT_POINT = {x: 663, y: 370}; //Pixels video 2
+    } else if (parseInt(videoNumber) === 3) {
+        //console.log("Entramos en 3");
+        TOP_LEFT_POINT = {x: 663, y: 718}; //Pixels video 3
+    }
+}
 
+$(document).ready(function () {
+    //seleccionamos el video segun lo que trae del html    
+    //selectVideo();  
     video = document.getElementById('video-player');
 
     $(video).ready(function () {
@@ -114,21 +173,21 @@ $(document).ready(function () {
     });
 
     $(video).resize(function () {
-
+        //TODO al hacer resize deberiamos ajustar canvas, pero no rula, FIX
+//        console.log("Entramos en resize");
+        // adaptCanvas();       
         if (!systemStarted) {
-
             //Create the videoDetections object from the xml result and add its observers
             my_manager = new ZoomManager(video, document.getElementById("zoom-layer"));
-
             systemStarted = true;
             bindVideoEvents(video);
-
             video.play();
         } else {
             //Here we have the necessary data of the video
             adjustCanvas(video);
         }
-        adjustCanvas(video);
+
+
     });
 
     $(window).resize(function () {
